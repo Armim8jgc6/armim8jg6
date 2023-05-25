5cf55354542c4067f2c3b2fb078c1066c8e44ceb
Armim8jgc6 <bed.armand@yahoo.com>
2023-05-25 19:21:47 +0200

    Írjatok egy REST végpontot, ahol id alapján lehet változtatni a futó szponzorát. Elkészültem.

--- src/main/java/hu/gde/runnersdemo/SponsorEntity.java
+++ src/main/java/hu/gde/runnersdemo/SponsorEntity.java
@@ -1,4 +1,31 @@
 package hu.gde.runnersdemo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "Sponsor")
 public class SponsorEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @JsonIgnore
+    @ManyToMany
+    @JoinColumn(nullable = false)
+    private RunnerEntity runner;
+
+    @Column(name = "sponsor_type", nullable = false)
+    private String sponsorType;
+    public long getId() {return id;}
+    public void setId(long id) {this.id = id;}
+    public RunnerEntity getRunner(){
+        return runner;
+    }
+    public void setRunner(RunnerEntity runner){
+        this.runner = runner;
+    }
+    public String getSponsorType() {
+        return sponsorType;
+    }
+    public void setSponsorType(String sponsorType) {this.sponsorType = sponsorType;}
 }
